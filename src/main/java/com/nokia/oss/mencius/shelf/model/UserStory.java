bef561c50e4ef18bf8eca00c7667af46b6122fd9
Liu Jeffrey <eoodin@outlook.com>
2016-04-06 22:18:33 +0800

    Add hierarchy for user stories.

--- src/main/java/com/nokia/oss/mencius/shelf/model/UserStory.java
+++ src/main/java/com/nokia/oss/mencius/shelf/model/UserStory.java
@@ -1,8 +1,14 @@
 package com.nokia.oss.mencius.shelf.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("US")
@@ -10,11 +16,34 @@ public class UserStory extends WorkItem {
     @Column
     private int points;
 
+    @JsonIgnore
+    @ManyToOne
+    private UserStory parent;
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.EAGER)
+    private List<UserStory> children;
+
     public int getPoints() {
         return points;
     }
 
     public void setPoints(int points) {
         this.points = points;
+    }
+
+    public UserStory getParent() {
+        return parent;
+    }
+
+    public void setParent(UserStory parent) {
+        this.parent = parent;
+    }
+
+    public List<UserStory> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<UserStory> children) {
+        this.children = children;
     }
 }
