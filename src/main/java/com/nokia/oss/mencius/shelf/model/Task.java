b1cb733151ebe73be83719253ecebe7849132917
Liu Jeffrey <eoodin@outlook.com>
2016-05-13 07:57:49 +0800

    set remaining to 0 when mark as finished.

--- src/main/java/com/nokia/oss/mencius/shelf/model/Task.java
+++ src/main/java/com/nokia/oss/mencius/shelf/model/Task.java
@@ -6,5 +6,10 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("TA")
 public class Task extends WorkItem {
-
+    @Override
+    public void setStatus(Status status) {
+        super.setStatus(status);
+        if (Status.Finished.equals(status))
+            setEstimation(0);
+    }
 }
