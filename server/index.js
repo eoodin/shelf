64d61dd9f2ddd3fd1c70409558a373ef65f5ffb9
Liu Jeffrey <eoodin@outlook.com>
2016-07-13 14:06:54 +0800

    move secuirty code out of index.jsp

--- server/index.js
+++ server/index.js
@@ -3,15 +3,12 @@
 module.exports = function(app) {
     var fs = require("fs"),
       express = require('express'),
-      cookieParser = require('cookie-parser'),
       bodyParser = require('body-parser'),
-      methodOverride = require('method-override'),
-      session = require('express-session'),
-      passport = require('passport'),
-      LocalStrategy = require('passport-local').Strategy;
+      methodOverride = require('method-override');
+      
     var models = require('../models');
 
-    app.use(cookieParser());
+
     app.use(bodyParser.urlencoded({ extended: false }));
     // app.use(bodyParser.json({ type: 'application/*+json' }));
 
@@ -34,57 +31,12 @@ module.exports = function(app) {
         console.log('Database synchronized.')
     });
 
+    require('./secure.js')(app);
     app.use("/api", require('./route.js')());
+    
+    // TODO: 
     app.use('/lib', express.static(__dirname + '/../node_modules/'));
 
 
-    app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    var ldapConf = __dirname + '/ldap.json';
-
-    if(fs.existsSync(ldapConf)) {
-        console.log('ldap.json found, using ldap auth');
-        var LdapStrategy = require('passport-ldapauth');
-        var ldapSetting = require(ldapConf);
-        passport.use(new LdapStrategy(ldapSetting));
-    }
-    else {
-        passport.use('local', new LocalStrategy(function(username, password, done) {
-            return models.user.find({where: {userId: username}}).then(function(u) {
-                done(null, u);
-            }, function(error) {
-                console.log('Authenticate failed: ', error);
-            });
-        }));
-    }
-
-    passport.serializeUser(function(user, done) {
-        done(null, user);
-    });
-
-    passport.deserializeUser(function(obj, done){
-        done(null, obj);
-    });
     
-    app.all('/api/*', function(req, res, next) {
-        if (req.isAuthenticated()) { return next(); }
-        console.log('Unauthenticated access to ' + req.originalUrl);
-        res.status(403).send({error: 'Unauthenticated'});
-    });
-    
-    app.all('/', function(req, res, next) {
-      if (req.isAuthenticated()) { return next(); }
-      res.redirect('/login.html');
-    });
-    
-    app.post('/login', passport.authenticate('local', { failureRedirect: '/login.html' }), function(req, res) {
-        res.redirect('/');
-    });
-
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/login.html');
-    });
 };
---
+++ server/secure.js
@@ -0,0 +1,58 @@
+module.exports = function(app) {
+    var fs        = require('fs');
+    var cookieParser = require('cookie-parser');
+    var session = require('express-session');
+    var passport = require('passport');
+    var LocalStrategy = require('passport-local').Strategy;
+
+    app.use(cookieParser());
+    app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    var ldapConf = __dirname + '/ldap.json';
+
+    if(fs.existsSync(ldapConf)) {
+        console.log('ldap.json found, using ldap auth');
+        var LdapStrategy = require('passport-ldapauth');
+        var ldapSetting = require(ldapConf);
+        passport.use(new LdapStrategy(ldapSetting));
+    }
+    else {
+        passport.use('local', new LocalStrategy(function(username, password, done) {
+            return models.user.find({where: {userId: username}}).then(function(u) {
+                done(null, u);
+            }, function(error) {
+                console.log('Authenticate failed: ', error);
+            });
+        }));
+    }
+
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function(obj, done){
+        done(null, obj);
+    });
+    
+    app.all('/api/*', function(req, res, next) {
+        if (req.isAuthenticated()) { return next(); }
+        console.log('Unauthenticated access to ' + req.originalUrl);
+        res.status(403).send({error: 'Unauthenticated'});
+    });
+    
+    app.all('/', function(req, res, next) {
+      if (req.isAuthenticated()) { return next(); }
+      res.redirect('/login.html');
+    });
+    
+    app.post('/login', passport.authenticate('local', { failureRedirect: '/login.html' }), function(req, res) {
+        res.redirect('/');
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/login.html');
+    });
+}
