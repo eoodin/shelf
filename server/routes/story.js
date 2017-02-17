975dc0a13260615beaac7e90bed98b65f97bcf70
Liu Jeffrey <jeffrey.liu@nokia.com>
2017-02-17 16:19:19 +0800

    add story db api

---
+++ server/routes/story.js
@@ -0,0 +1,110 @@
+module.exports = function(router) {
+    var models = require('../models');
+    var csv = require('../modules/csv');
+    router.route('/stories')
+        .get(function(req, res) {
+            var ob = req.query.sortBy ? req.query.sortBy : 'id';
+            if (ob == 'owner') ob = 'ownerId';
+
+            if (req.query.desc) {
+                ob = [[ob, 'desc']]
+            }
+            let where = {};
+            if (req.query.projectId) {
+                where = {projectId: req.query.projectId};
+            }
+            
+            if (req.query.parent) {
+                where['parentId'] = req.query.parent == 'null' ? null : req.query.parent;
+            }
+
+            if (req.query.status) {
+                let status = req.query.status.split(',');
+                where['status'] = {$in : status};
+            }
+
+            models.story.findAll({
+                where: where,
+                // TODO: keep only id of owner and creator to reduce data size
+                include: [
+                    {model: models.user, as: 'creator'},
+                    {model: models.story, as: 'children'}
+                ],
+                order: ob
+            }).then(function(stories) {   
+                res.json(stories);
+            })
+        })
+        .post(function(req, res) {
+            if (!req.body.projectId) {
+                return res.sendStatus(500);
+            }
+
+            models.user.findById(req.user.id).then(function(u) {
+                let def = {
+                    parentId: req.body.parentId,
+                    status: 'New',
+                    title: req.body.title,
+                    description: req.body.description,
+                    creatorId: u.id,
+                    projectId: req.body.projectId,
+                    points: req.body.points
+                };
+                var story = models.story.build(def);
+                story.save().then(function (story) {
+                    res.json(story.id);
+                })
+            }).catch(function(errors){
+                console.log("Error: " + JSON.stringify(errors));
+                res.sendStatus(500);
+            });
+        });
+
+    router.route('/stories/:id')
+        .get(function(req, res) {
+            models.story.findById(req.params.id).then(function(story) {
+                res.json(story);
+            }).catch(function(errors){
+                console.log("Error: " + JSON.stringify(errors));
+                res.sendStatus(500);
+            });
+        })
+        .patch(function(req, res) {
+            models.story.findById(req.params.id).then(function(story) {
+                var origin = {};
+                var changes = {};
+                for(let f in req.body) {
+                    if (f == 'updatedAt' || f == 'createdAt') {
+                        continue;
+                    }
+
+                    if (story[f] != req.body[f]) {
+                        origin[f] = story[f];
+                        changes[f] = req.body[f];
+                    }
+                }
+
+                story.update(changes).then(function(story) {
+                    console.log('Story changed');
+                    res.json(story);
+                });
+            }).catch(function(errors){
+                console.log("Error: " + JSON.stringify(errors));
+                res.sendStatus(500);
+            });
+        })
+        .delete(function(req, res){
+            models.story.findById(req.params.id).then(function(story) {
+                story.destroy().then(function(story) {
+                    res.json(req.params.id);
+                })
+                .catch(function(errors){
+                    console.log("Error: " + JSON.stringify(errors));
+                    res.sendStatus(500);
+                });
+            }).catch(function(errors){
+                console.log("Error: " + JSON.stringify(errors));
+                res.sendStatus(500);
+            });
+        });
+};
