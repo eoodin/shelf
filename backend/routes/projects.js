7623b39c49fa136ec7348764ca26427d5f1adf85
Liu Jeffrey <jeffrey.liu@nokia.com>
2017-10-18 08:32:59 +0800

    exclude releases in projects list api

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -2,7 +2,7 @@ module.exports = function(router) {
     var models = require('../models');
 
     router.route('/projects').get(function(req, res){
-        models.project.findAll({include: [models.team, models.release]})
+        models.project.findAll({include: [models.team]})
             .then(function(projects) {
                 res.json(projects);
         });
