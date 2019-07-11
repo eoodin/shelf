40aecf5ca8c297907448ed469145964bc05d65fd
Liu Jeffrey <eoodin@outlook.com>
2019-07-11 09:10:14 +0800

    add base tables in migration, fix typo

--- backend/migrations/20170923120735-create-change-histories.js
+++ backend/migrations/20170923120735-create-change-histories.js
@@ -1,68 +1,62 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    console.log('creating defect_histories');
-    return queryInterface.createTable('defect_histories', {
-      historyId: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.BIGINT
-      },
-      defectId: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.BIGINT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    }).then(function() {
-      console.log('creating task_histories');
-      return queryInterface.createTable('task_histories', {
-        taskId: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.BIGINT
-        },
-        historyId: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.BIGINT
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE
-        }
-      })
-    }).then(() => {
-      console.log('creating story_histories');
-      return queryInterface.createTable('story_histories', {
-        storyId: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.BIGINT
-        },
-        historyId: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.BIGINT
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE
-        }
-      });
-    });
+    return Promise.all([
+        queryInterface.createTable('defect_histories', {
+            historyId: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.BIGINT
+            },
+            defectId: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.BIGINT
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        }),
+        queryInterface.createTable('task_histories', {
+            taskId: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.BIGINT
+            },
+            historyId: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.BIGINT
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        }),
+        queryInterface.createTable('story_histories', {
+            storyId: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.BIGINT
+            },
+            historyId: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.BIGINT
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        }),
+    ]);
   },
   down: (queryInterface, Sequelize) => {
-    console.log('dropping defect_histories');
-    return queryInterface.dropTable('defect_histories').then(() => {
-      console.log('dropping task_histories');
-      return queryInterface.dropTable('task_histories');
-    }).then(() => {
-      console.log('dropping story_histories');
-      return queryInterface.dropTable('story_histories');
-    });
+      return Promise.all([
+          queryInterface.dropTable('defect_histories'),
+          queryInterface.dropTable('task_histories'),
+          queryInterface.dropTable('story_histories')
+      ]);
   }
 };
