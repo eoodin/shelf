682731ca0aeb32c752426b74790d143824425afb
Liu Jeffrey <eoodin@outlook.com>
2020-01-03 11:30:44 +0800

    change request mapping for items to get

--- server/src/main/java/online/eoodin/shelf/rest/ItemController.java
+++ server/src/main/java/online/eoodin/shelf/rest/ItemController.java
@@ -4,7 +4,7 @@ import online.eoodin.shelf.dao.WorkItemDAO;
 import online.eoodin.shelf.entity.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +22,7 @@ public class ItemController {
         this.workItemDAO = workItemDAO;
     }
 
-    @RequestMapping("/items")
+    @GetMapping("/items")
     public List<Item> getItems(@RequestParam(value = "page", defaultValue = "1") Integer page) {
         List<Item> list = new ArrayList<>();
         workItemDAO.findAll(PageRequest.of(page - 1, 20)).forEach(list::add);
